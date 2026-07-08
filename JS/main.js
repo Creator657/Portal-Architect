342d81acdfb246161ea6902e1c112ff2fea0b27c
Creator657 <Killyrye@gmail.com>
2026-07-08 00:39:00 -0700

    Add button click event to generate portal output

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,26 @@
-console.log("Portal Architect loaded!");
+console.log("Portal Architect loaded");
+
+const button = document.getElementById("generateBtn");
+
+button.addEventListener("click", function(){
+
+    const x = document.getElementById("motherX").value;
+
+    const z = document.getElementById("motherZ").value;
+
+    const radius = document.getElementById("radius").value;
+
+
+    document.getElementById("output").innerHTML = `
+
+    <h2>Mother Portal</h2>
+
+    <p>X: ${x}</p>
+
+    <p>Z: ${z}</p>
+
+    <p>Radius: ${radius}</p>
+
+    `;
+
+});
